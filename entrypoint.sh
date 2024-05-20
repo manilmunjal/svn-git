cbd17c884cb1cdfa839c62c8051b8989c5888e30
Manil munjal <111218259+manilmunjal@users.noreply.github.com>
2024-05-20 17:06:25 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,12 @@
 
 set -e
 
+# Create .netrc file for SVN authentication
+echo "machine $(echo $SVN_REPO_URL | awk -F/ '{print $3}') login $SVN_USERNAME password $SVN_PASSWORD" > ~/.netrc
+chmod 600 ~/.netrc
+
 # Clone the SVN repository
-git svn clone $SVN_REPO_URL --stdlayout --username $SVN_USERNAME --password $SVN_PASSWORD svn-repo
+git svn clone $SVN_REPO_URL --stdlayout svn-repo
 cd svn-repo
 
 # Configure Git
@@ -11,7 +15,7 @@ git config --global user.name "$GIT_USERNAME"
 git config --global user.email "$GIT_EMAIL"
 
 # Add the remote repository
-git remote add origin https://$GIT_USERNAME:$GIT_TOKEN@$GIT_REPO_URL
+git remote add origin https://$GIT_USERNAME:$GIT_TOKEN@$(echo $GIT_REPO_URL | awk -F/ '{print $3}')/${GIT_REPO_URL##*/}
 
 # Push the code to the remote repository
 git push -u origin --all
